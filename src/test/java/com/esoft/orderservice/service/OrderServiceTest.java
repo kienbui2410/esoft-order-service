59400fe712981b466fcb563b0d736800bc5ee5c5
kienbui2410 <nemoken2410@gmail.com>
2023-07-04 18:46:46 +0700

    update unit test case list order pagination

--- src/test/java/com/esoft/orderservice/service/OrderServiceTest.java
+++ src/test/java/com/esoft/orderservice/service/OrderServiceTest.java
@@ -3,6 +3,7 @@ package com.esoft.orderservice.service;
 import com.esoft.orderservice.common.DateUtil;
 import com.esoft.orderservice.model.Order;
 import com.esoft.orderservice.model.User;
+import com.esoft.orderservice.model.payload.OrderResponse;
 import com.esoft.orderservice.repo.OrderRepo;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,11 +14,18 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OrderServiceTest {
@@ -164,5 +172,79 @@ public class OrderServiceTest {
             System.out.println(e.getMessage());
             assertThat(e.getMessage(), startsWith("Order is not belong to the user"));
         }
+    }
+
+    @Test
+    public void testListOrder_returnsOrderPagination() {
+
+        Order mockOrder = Order.builder()
+                .id(1L)
+                .userId(1L)
+                .createdAt(new Date())
+                .category(Order.Category.SUPER_LUXURY)
+                .quantity(2L)
+                .serviceName(Order.ServiceName.VIDEO_EDITING)
+                .desc("unit test")
+                .note("unit test")
+                .price(new BigDecimal("100000"))
+                .build();
+        List<Order> orderList = new ArrayList<>();
+        orderList.add(mockOrder);
+        orderList.add(mockOrder);
+        orderList.add(mockOrder);
+        orderList.add(mockOrder);
+        orderList.add(mockOrder);
+
+        Page<Order> page = new PageImpl(orderList, new Pageable() {
+            @Override
+            public int getPageNumber() {
+                return 0;
+            }
+
+            @Override
+            public int getPageSize() {
+                return 5;
+            }
+
+            @Override
+            public long getOffset() {
+                return 0;
+            }
+
+            @Override
+            public Sort getSort() {
+                return null;
+            }
+
+            @Override
+            public Pageable next() {
+                return null;
+            }
+
+            @Override
+            public Pageable previousOrFirst() {
+                return null;
+            }
+
+            @Override
+            public Pageable first() {
+                return null;
+            }
+
+            @Override
+            public boolean hasPrevious() {
+                return false;
+            }
+        }, 11);
+
+        Mockito.when(orderRepo.findAll(Mockito.any(Pageable.class))).thenReturn(page);
+
+        OrderResponse orderResponse = orderService.list(0, 5, "id", "asc");
+        System.out.println(orderResponse);
+        assertThat(orderResponse.getContent().size(), is(5));
+        assertThat(orderResponse.getPageNo(), is(0));
+        assertThat(orderResponse.getPageSize(), is(5));
+        assertThat(orderResponse.getTotalElements(), is(11L));
+        assertThat(orderResponse.getTotalPages(), is(3));
     }
 }
