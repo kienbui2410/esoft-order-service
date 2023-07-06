f5ff52f567752890037df8c1129bbe692da16e09
kienbui2410 <nemoken2410@gmail.com>
2023-07-06 21:50:38 +0700

    add controller test

--- src/test/java/com/esoft/orderservice/controller/ReportControllerTest.java
+++ src/test/java/com/esoft/orderservice/controller/ReportControllerTest.java
@@ -1,5 +1,6 @@
 package com.esoft.orderservice.controller;
 
+import com.esoft.orderservice.common.DateUtil;
 import com.esoft.orderservice.model.CustomUserDetails;
 import com.esoft.orderservice.model.Order;
 import com.esoft.orderservice.model.User;
@@ -37,7 +38,7 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @SpringBootTest
-public class OrderControllerTest {
+public class ReportControllerTest {
 
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -69,102 +70,7 @@ public class OrderControllerTest {
 
 
     @Test
-    public void orderController_whenCreateOrder_thenReturnCreatedOrder() throws Exception {
-
-        Order mockOrder = Order.builder()
-                .category(Order.Category.SUPER_LUXURY)
-                .quantity(2L)
-                .serviceName(Order.ServiceName.VIDEO_EDITING)
-                .desc("unit test")
-                .note("unit test")
-                .price(new BigDecimal("100000"))
-                .build();
-
-        ResultActions responseBody = this.mockMvc.perform(MockMvcRequestBuilders.post("/orders")
-                .with(sessionUser(userDetails))
-                .content(asJsonString(mockOrder))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON));
-
-        ResponseMessage response = this.mapper.readValue(responseBody.andReturn().getResponse().getContentAsString(), ResponseMessage.class);
-        LinkedHashMap<String,Object> data = (LinkedHashMap<String,Object>)response.getData();
-        assertTrue(response.getCode().equals("200"));
-        assertNotNull(data.get("id"));
-        assertTrue(data.get("userId").toString().equals("2"));
-        assertTrue(((String)data.get("ref")).contains("ODR-"));
-    }
-
-    @Test
-    public void orderController_whenUpdateOrder_thenReturnUpdatedOrder() throws Exception {
-
-        Order mockOrder = Order.builder()
-                .category(Order.Category.SUPER_LUXURY)
-                .quantity(2L)
-                .serviceName(Order.ServiceName.VIDEO_EDITING)
-                .desc("unit test")
-                .note("unit test")
-                .price(new BigDecimal("100000"))
-                .build();
-
-        orderService.create(mockOrder, userDetails.getUser());
-
-        Order mockUpdateOrder = Order.builder()
-                .id(1L)
-                .userId(2L)
-                .ref("ODR-20230620-1")
-                .category(Order.Category.SUPREME_LUXURY)
-                .quantity(2L)
-                .serviceName(Order.ServiceName.PHOTO_EDITING)
-                .createdAt(new Date())
-                .desc("unit test updated")
-                .note("unit test updated")
-                .price(new BigDecimal("200000"))
-                .build();
-
-        ResultActions responseBody = this.mockMvc.perform(MockMvcRequestBuilders.put("/orders")
-                .with(sessionUser(userDetails))
-                .content(asJsonString(mockUpdateOrder))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON));
-        System.out.println(responseBody);
-        ResponseMessage response = this.mapper.readValue(responseBody.andReturn().getResponse().getContentAsString(), ResponseMessage.class);
-        LinkedHashMap<String,Object> data = (LinkedHashMap<String,Object>)response.getData();
-        assertTrue(response.getCode().equals("200"));
-        assertTrue(data.get("category").equals(Order.Category.SUPREME_LUXURY.toString()));
-        assertTrue(data.get("serviceName").equals(Order.ServiceName.PHOTO_EDITING.toString()));
-        assertTrue(data.get("desc").equals("unit test updated"));
-        assertTrue(data.get("note").equals("unit test updated"));
-        assertTrue(((Integer)data.get("price")) == 200000);
-
-    }
-
-    @Test
-    public void orderController_whenDeleteOrder_thenReturn200Ok() throws Exception {
-
-        Order mockOrder = Order.builder()
-                .category(Order.Category.SUPER_LUXURY)
-                .quantity(2L)
-                .serviceName(Order.ServiceName.VIDEO_EDITING)
-                .desc("unit test")
-                .note("unit test")
-                .price(new BigDecimal("100000"))
-                .build();
-
-        Order orderDb = orderService.create(mockOrder, userDetails.getUser());
-
-        ResultActions responseBody = this.mockMvc.perform(MockMvcRequestBuilders.delete("/orders/"+orderDb.getId())
-                .with(sessionUser(userDetails))
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON));
-        List<Order> list = orderService.list();
-
-        ResponseMessage response = this.mapper.readValue(responseBody.andReturn().getResponse().getContentAsString(), ResponseMessage.class);
-        assertTrue(response.getCode().equals("200"));
-        assertEquals(list.size(), 0);
-    }
-
-    @Test
-    public void orderController_whenListOrder_thenOrderList() throws Exception {
+    public void reportController_whenCreateOrder_thenReturnNumberOfOrder() throws Exception {
 
         Order mockOrder = Order.builder()
                 .category(Order.Category.SUPER_LUXURY)
@@ -190,51 +96,105 @@ public class OrderControllerTest {
                 .note("unit test 3")
                 .price(new BigDecimal("100000"))
                 .build();
-        Order mockOrder4 = Order.builder()
+        orderService.create(mockOrder, userDetails.getUser());
+        orderService.create(mockOrder2, userDetails.getUser());
+        orderService.create(mockOrder3, userDetails.getUser());
+
+        ResultActions responseBody = this.mockMvc.perform(MockMvcRequestBuilders.get("/reports/users/2/order-number")
+                .with(sessionUser(userDetails))
+                .content(asJsonString(mockOrder))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        ResponseMessage response = this.mapper.readValue(responseBody.andReturn().getResponse().getContentAsString(), ResponseMessage.class);
+        assertTrue(((Integer)response.getData()) == 3);
+    }
+
+    @Test
+    public void reportController_whenCreateOrder_thenReturnRevenue() throws Exception {
+
+        Order mockOrder = Order.builder()
                 .category(Order.Category.SUPER_LUXURY)
-                .quantity(2L)
+                .quantity(1L)
                 .serviceName(Order.ServiceName.VIDEO_EDITING)
-                .desc("unit test 4")
-                .note("unit test 4")
+                .desc("unit test")
+                .note("unit test")
                 .price(new BigDecimal("100000"))
                 .build();
-        Order mockOrder5 = Order.builder()
+        Order mockOrder2 = Order.builder()
                 .category(Order.Category.SUPER_LUXURY)
                 .quantity(2L)
                 .serviceName(Order.ServiceName.VIDEO_EDITING)
-                .desc("unit test 5")
-                .note("unit test 5")
-                .price(new BigDecimal("100000"))
+                .desc("unit test 2")
+                .note("unit test 2")
+                .price(new BigDecimal("200000"))
                 .build();
-        Order mockOrder6 = Order.builder()
+        Order mockOrder3 = Order.builder()
                 .category(Order.Category.SUPER_LUXURY)
-                .quantity(2L)
+                .quantity(3L)
                 .serviceName(Order.ServiceName.VIDEO_EDITING)
-                .desc("unit test 6")
-                .note("unit test 6")
-                .price(new BigDecimal("100000"))
+                .desc("unit test 3")
+                .note("unit test 3")
+                .price(new BigDecimal("300000"))
                 .build();
         orderService.create(mockOrder, userDetails.getUser());
         orderService.create(mockOrder2, userDetails.getUser());
         orderService.create(mockOrder3, userDetails.getUser());
-        orderService.create(mockOrder4, userDetails.getUser());
-        orderService.create(mockOrder5, userDetails.getUser());
-        orderService.create(mockOrder6, userDetails.getUser());
 
-        ResultActions responseBody = this.mockMvc.perform(MockMvcRequestBuilders.get("/orders?pageNo=0&pageSize=5&sortBy=id&sortDir=asc")
+        ResultActions responseBody = this.mockMvc.perform(MockMvcRequestBuilders.get("/reports/users/2/revenue")
                 .with(sessionUser(userDetails))
+                .content(asJsonString(mockOrder))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON));
 
-        List<Order> list = orderService.list();
         ResponseMessage response = this.mapper.readValue(responseBody.andReturn().getResponse().getContentAsString(), ResponseMessage.class);
-        LinkedHashMap<String,Object> data = (LinkedHashMap<String,Object>)response.getData();
-        assertTrue(response.getCode().equals("200"));
-        assertEquals(data.get("totalPages"), 2);
-        assertEquals(data.get("totalElements"), 6);
-        assertEquals(list.size(), 6);
+        //1*100000 + 2*200000 + 3*300000 = 1400000
+        assertTrue(((Integer)response.getData()) == 1400000);
     }
 
+    @Test
+    public void reportController_whenCreateOrder_thenReturnInfoByTime() throws Exception {
+
+        Order mockOrder = Order.builder()
+                .category(Order.Category.SUPER_LUXURY)
+                .quantity(1L)
+                .serviceName(Order.ServiceName.VIDEO_EDITING)
+                .desc("unit test")
+                .note("unit test")
+                .price(new BigDecimal("100000"))
+                .build();
+        Order mockOrder2 = Order.builder()
+                .category(Order.Category.SUPER_LUXURY)
+                .quantity(2L)
+                .serviceName(Order.ServiceName.VIDEO_EDITING)
+                .desc("unit test 2")
+                .note("unit test 2")
+                .price(new BigDecimal("200000"))
+                .build();
+        Order mockOrder3 = Order.builder()
+                .category(Order.Category.SUPER_LUXURY)
+                .quantity(3L)
+                .serviceName(Order.ServiceName.VIDEO_EDITING)
+                .desc("unit test 3")
+                .note("unit test 3")
+                .price(new BigDecimal("300000"))
+                .build();
+        orderService.create(mockOrder, userDetails.getUser());
+        orderService.create(mockOrder2, userDetails.getUser());
+        orderService.create(mockOrder3, userDetails.getUser());
+
+        ResultActions responseBody = this.mockMvc.perform(MockMvcRequestBuilders.get("/reports/order-revenue-summary/year/2023/month/7")
+                .with(sessionUser(userDetails))
+                .content(asJsonString(mockOrder))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON));
+
+        ResponseMessage response = this.mapper.readValue(responseBody.andReturn().getResponse().getContentAsString(), ResponseMessage.class);
+        LinkedHashMap<String,Object> data = (LinkedHashMap<String,Object>)response.getData();
+        //1*100000 + 2*200000 + 3*300000 = 1400000
+        assertTrue(((Integer)data.get("noOfOrder")) == 3);
+        assertTrue(((Integer)data.get("revenue")) == 1400000);
+    }
 
 
     public static String asJsonString(final Object obj) {
